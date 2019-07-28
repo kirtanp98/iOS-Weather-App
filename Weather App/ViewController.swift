341619de3858c1ad5a9becdb839b1a094d2e9f39
Kirtan Patel <kirtanpatl@gmail.com>
2019-07-28 17:14:04 -0400

    Add a method to fetch the JSON response

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -7,22 +7,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        if let url = URL(string: "https://api.darksky.net/forecast/6fbcf7b85b9cd3021c8a39883c63389d/37.8267,-122.4233") {
+        let weather = fetchWeather(longitute: 37.8267, latitude: -122.4233)
+    }
+
+    
+    //TODO: Make this in a delegate
+    //Because we are not gaurentee to get the response and because it is a async task so we have to check if its done
+    func fetchWeather(longitute: Double, latitude : Double) -> WeatherModel? {
+        let urlString = "https://api.darksky.net/forecast/6fbcf7b85b9cd3021c8a39883c63389d/\(longitute),\(latitude)"
+        var weatherObject : WeatherModel? = nil
+        
+        if let url = URL(string: urlString){
             URLSession.shared.dataTask(with: url) { data, response, error in
                 if let data = data {
                     do {
-                        let res = try JSONDecoder().decode(WeatherModel.self, from: data)
-                        print(res.latitude)
-                        print(res.longitude)
+                        weatherObject = try JSONDecoder().decode(WeatherModel.self, from: data)
                     } catch let error {
                         print(error)
                     }
                 }
                 }.resume()
+            return weatherObject
         }
+        return nil
     }
-
-
+    
 }
 
