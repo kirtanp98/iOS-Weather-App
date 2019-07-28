93c6e1fe7fd81f3b3be699984d66be5d084fa26a
zain08816 <zain08816@gmail.com>
2019-07-28 17:48:07 -0600

    Add Location Services to get latitude and longitude

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -1,16 +1,70 @@
 
 
 import UIKit
-import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkLocationServices()
+        
+        
         let weather = fetchWeather(longitute: 37.8267, latitude: -122.4233)
-    }
 
+        
+    }
     
+    
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            // Turn on
+        }
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.startUpdatingLocation()
+        print("Location Manger Setup")
+    }
+    
+    func checkLocationAuthorization() {
+        print("Location Authorization Check")
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            print(getLatLong())
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            checkLocationAuthorization()
+        case .restricted:
+            break
+        case .authorizedAlways:
+            break
+        case .denied:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    func getLatLong() -> Array<Any> {
+        let currentLocation = locationManager.location
+        let longitude = currentLocation?.coordinate.longitude as Any
+        let latitude = currentLocation?.coordinate.latitude as Any
+        
+        return [latitude, longitude]
+    }
+    
+    
+ 
+
     //TODO: Make this in a delegate
     //Because we are not gaurentee to get the response and because it is a async task so we have to check if its done
     func fetchWeather(longitute: Double, latitude : Double) -> WeatherModel? {
