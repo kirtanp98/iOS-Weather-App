ec9945747535f29915ee55849544a4f4bbee4f6b
Kirtan Patel <kirtanpatl@gmail.com>
2019-07-28 19:49:01 -0400

    Add a delegate

--- Weather App/WeatherModel.swift
+++ Weather App/WeatherModel.swift
@@ -2,19 +2,19 @@
 
 import Foundation
 
-class WeatherModel : Codable {
+public class WeatherModel : Codable {
     let latitude : Double
     let longitude: Double
     let currentWeather : currentWeatherModel
     
-    class currentWeatherModel : Codable {
+    public class currentWeatherModel : Codable {
         let time : Date
         let summary : String = ""
         let icon : String = ""
         let precipProbability : Double = 0.0
-        let precipitaionType : String?
+        let precipitaionType : String? = nil
         let temperature : Double = 0.0 //Degrees Celcius
-        let visibility : Double = 0.0 //Kilometers
+        let visibility : Double = 0.0//Kilometers
 
         enum CodingKeys: String, CodingKey {
             case time
